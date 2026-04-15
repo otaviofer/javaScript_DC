cc2c0c65d8994d4ece0df88dca278cbe61f46c4a
Otavio Fernandes da Silva dos Santos <otaviofernandes113@gmail.com>
2026-04-15 20:42:54 -0300

    feat: manipulando elementos atraves do ID

--- modulo_doom/js/script.js
+++ modulo_doom/js/script.js
@@ -1,20 +1,13 @@
-// ** MANIPULANDO ELEMENTOS E CONTEÚDO
+// ** MANIPULANDO CSS ATRAVÉS DO ID
 
-const p = document.getElementsByTagName('p'); // pegando todas as tags do mesmo tipo
+const el = document.getElementById('box');  // buscando elemento pelo ID
+el.style.width = 100;
+el.style.height = 100;
+el.style.backgroundColor = 'red';
+el.style.marginLeft = 200;
 
-// acessando via posição da tag
-// alert(p[3].innerHTML);
+// CSS: margin-Left
+// JS: marginLeft
 
-// alterando elemento da tag via JavaScript
-// p[0].innerHTML = 'Manipulado via JS!';
-
-/*
-for(var i=0; i<p.length; i++){
-    p[i].innerHTML = 'Manipulado via JS - ' + i;
-}
-*/
-
-// pegando o conteúdo já existente e adicionando algo a ele
-for(i=0; i<10; i++){
-    p[0].innerHTML = p[0].innerHTML + i;
-}
+// CSS: backgroundColor
+// JS: backgroundColor
