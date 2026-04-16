73e30cfdafd7f68618e41a29d8b3ba03e2aed1cb
Otavio Fernandes da Silva dos Santos <otaviofernandes113@gmail.com>
2026-04-15 22:21:03 -0300

    feat: estrutura inicial do slider: HTML, CSS

--- modulo_doom/js/script.js
+++ modulo_doom/js/script.js
@@ -1,16 +1,1 @@
-// ** ANIMAÇÕES COM JAVASCRIPT PT2
-
-const span = document.querySelector('.conteudo span')
-
-span.addEventListener("click", function(){
-    const conteudo = document.querySelector('.conteudo p');
-
-    if(conteudo.classList.contains('mostrar')){
-        span.innerHTML = 'Ver mais...';
-        conteudo.classList.remove('mostrar');
-    }else{
-        span.innerHTML = 'Ver menos.';
-        conteudo.classList.add('mostrar');
-    }
-
-});
+// ** CRIANDO SLIDER COM VANILLA JS
