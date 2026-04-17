96549dbb1d697473b671af33439b0edef66cf41c
Otavio Fernandes da Silva dos Santos <otaviofernandes113@gmail.com>
2026-04-17 00:19:38 -0300

    feat: template strings

---
+++ modulo_jsModerno/js/script.js
@@ -0,0 +1,20 @@
+/** TEMPLATE STRINGS 
+ * 
+ *  possibilidade de quebrar linhas
+ * mais facilidade para concatenar
+*/
+
+const nome = 'otavio';
+const conteudo = `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada enim scelerisque, fermentum magna et, tempor nunc. 
+    Ut venenatis tempor quam, id venenatis augue blandit a. Vestibulum sit amet ipsum in ipsum accumsan ultricies id ut metus. 
+    Praesent ut turpis vel nisi rhoncus congue. Maecenas lacinia nunc nec pharetra eleifend. Nulla suscipit mi sapien, quis tincidunt ex facilisis ac. 
+    Curabitur justo tellus, semper hendrerit erat nec, gravida rutrum lectus. Nam a varius odio. Proin eleifend leo quis tristique pretium.
+
+    Meu nome é ${nome}!
+
+
+
+`;
+
+console.log(conteudo);
